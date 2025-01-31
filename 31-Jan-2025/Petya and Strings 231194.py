25b855457dab4c4a44d45d20fdd8b6360bd6ee02
Natnael Yohanes <mataniayohanes@gmail.com>
2025-01-31 17:15:23 +0300

    Created 'Petya and Strings' by A2SV Hub

---
+++ 31-Jan-2025/Petya and Strings 231194.py
@@ -0,0 +1,39 @@
+# Problem: Petya and Strings - https://codeforces.com/problemset/problem/112/A
+
+from __future__ import division, print_function
+import itertools
+import sys
+import os
+
+if sys.version_info[0] < 3:
+    input = raw_input
+    range = xrange
+
+    filter = itertools.ifilter
+    map = itertools.imap
+    zip = itertools.izip
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def solve(n,m):
+    n = n.lower()
+    m = m.lower()
+    if n < m:
+        return -1
+    elif  n > m:
+        return 1
+    else:
+        return 0
+    
+
+
+def main():
+    n = input()
+    m = input()
+    print(solve(n,m))
+
+if __name__ == "__main__":
+    main()
