411f5c9fb09c189dc15e3c2c8cd18e34c4884cfe
Natnael Yohanes <mataniayohanes@gmail.com>
2025-01-31 18:16:24 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 31-Jan-2025/Love Story 231370.py
@@ -0,0 +1,37 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+from __future__ import division, print_function
+import itertools
+import sys
+import os
+
+if sys.version_info[0] < 3:
+    input = raw_input
+    range = xrange
+
+    filter = itertools.ifilter
+    map = itertools.imap
+    zip = itertools.izip
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def solve(s):
+    default = "codeforces"
+    count = 0
+    for i,char in enumerate(s):
+        if default[i] != char:
+            count += 1
+    return count
+
+
+def main():
+    t = int(input().strip())
+    for _ in range(t):
+        s = input().strip()
+        print(solve(s))
+
+if __name__ == "__main__":
+    main()
