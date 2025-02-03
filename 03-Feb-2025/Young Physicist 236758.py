a8f744a5cb37950bbc27a7bf87846bca1aa05d0c
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-03 17:49:32 +0300

    Created 'Young Physicist' by A2SV Hub

---
+++ 03-Feb-2025/Young Physicist 236758.py
@@ -0,0 +1,39 @@
+# Problem: Young Physicist - https://codeforces.com/problemset/problem/69/A
+
+from __future__ import division, print_function
+import itertools
+import sys
+import os
+
+if sys.version_info[0] < 3:
+    input = raw_input
+    range = xrange
+
+    filter = itertools.ifilter
+    map = itertools.imap
+    zip = itertools.izip
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def solve():
+    n = int(input())
+    sum_x = sum_y = sum_z = 0
+    for _ in range(n):
+        x, y, z = map(int, input().split())
+        sum_x += x
+        sum_y += y
+        sum_z += z
+
+    if sum_x == 0 and sum_y == 0 and sum_z == 0:
+        print("YES")
+    else:
+        print("NO")
+
+def main():
+    solve()
+
+if __name__ == "__main__":
+    main()
