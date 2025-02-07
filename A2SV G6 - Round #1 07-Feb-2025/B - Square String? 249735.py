5dbab6f5cf9284dd2e9dc92fa62c38ee46a9c6e5
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-07 15:25:19 +0300

    Created 'B - Square String?' by A2SV Hub

---
+++ A2SV G6 - Round #1 07-Feb-2025/B - Square String? 249735.py
@@ -0,0 +1,34 @@
+# Problem: B - Square String? - https://codeforces.com/gym/585107/problem/B
+
+from __future__ import division, print_function
+import itertools
+import sys
+import os
+
+if sys.version_info[0] < 3:
+    input = raw_input
+    range = xrange
+
+    filter = itertools.ifilter
+    map = itertools.imap
+    zip = itertools.izip
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def solve(s):
+    if len(s) == 1:
+        return "NO"
+    half = len(s) // 2 if len(s) % 2 == 0 else len(s) // 2 + 1
+    return "YES" if s[:half] == s[half:] else "NO"
+
+def main():
+    t = int(input().strip())
+    for _ in range(t):
+        s = input().strip()
+        print(solve(s))
+
+if __name__ == "__main__":
+    main()
