60a0067af75f275eeed4aac7882f1f93deae05a0
Natnael Yohanes <mataniayohanes@gmail.com>
2025-02-11 10:32:57 +0300

    Created 'Insertion Sort' by A2SV Hub

---
+++ 11-Feb-2025/Insertion Sort 255264.py
@@ -0,0 +1,18 @@
+# Problem: Insertion Sort - https://www.hackerrank.com/challenges/insertionsort1/problem
+
+def insertionSort1(n, arr):
+    key = arr[-1]
+    i = n - 2
+    while (i >= 0 and arr[i] > key):
+        arr[i + 1] = arr[i]
+        print(" ".join(map(str, arr)))
+        i -= 1
+    arr[i + 1] = key
+    print(" ".join(map(str, arr)))
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
