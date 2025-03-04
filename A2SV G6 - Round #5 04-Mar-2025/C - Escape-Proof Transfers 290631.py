c46bc678d4008249138eaec1715d4de818a7be28
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-04 10:15:04 +0300

    Created 'C - Escape-Proof Transfers' by A2SV Hub

---
+++ A2SV G6 - Round #5 04-Mar-2025/C - Escape-Proof Transfers 290631.py
@@ -0,0 +1,52 @@
+# Problem: C - Escape-Proof Transfers - https://codeforces.com/gym/591928/problem/C
+
+import itertools
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def iinp():
+    return int(input().strip())
+
+def linp():
+    return list(map(int, input().strip().split()))
+
+def string():
+    return input().strip()
+
+def lsinp():
+    return input().strip().split()
+
+def digit():
+    return [int(i) for i in input().strip()]
+
+def character():
+    return list(input().strip())
+
+def solve(n,t,c,pr):
+    count = 0
+    l = 0
+    r = 0
+    while r < n:
+        if l < n and pr[r] > t:
+            l = r + 1
+        if l < n and r - l + 1 == c:
+            count += 1
+            l += 1
+        r += 1
+    return count
+
+
+def main():
+    # n = iinp()
+    n, t, c = linp()
+    pr = linp()
+    print(solve(n,t,c,pr))
+
+if __name__ == '__main__':
+    main()
