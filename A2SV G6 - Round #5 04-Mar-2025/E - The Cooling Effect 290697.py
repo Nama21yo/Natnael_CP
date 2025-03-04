8c6a855c8adf3792c31284c907acc2c87fd06fdb
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-04 11:10:53 +0300

    Created 'E - The Cooling Effect' by A2SV Hub

---
+++ A2SV G6 - Round #5 04-Mar-2025/E - The Cooling Effect 290697.py
@@ -0,0 +1,54 @@
+# Problem: E - The Cooling Effect - https://codeforces.com/gym/591928/problem/E
+
+import itertools
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def iinp():
+    return int(input().strip())
+
+def linp():
+    return list(map(int, input().strip().split()))
+
+def string():
+    return input().strip()
+
+def lsinp():
+    return input().strip().split()
+
+def digit():
+    return [int(i) for i in input().strip()]
+
+def character():
+    return list(input().strip())
+
+def solve():
+    n, k  = linp()
+    a = [float("inf")] * n
+    index = linp()
+    point = linp()
+    for i in range(k):
+        a[index[i] - 1] = point[i]
+    b = a.copy()
+    # go from left
+    for l in range(1,n):
+        a[l] = min(a[l], a[l - 1] + 1)
+    # go from right
+    for r in range(n - 2, -1, -1):
+        b[r] = min(b[r], b[r + 1] + 1)
+    ans = [min(ai,bi) for ai,bi in zip(a,b)]
+    return ans
+def main():
+    t = iinp()
+    for _ in range(t):
+        space = linp()
+        print(*solve())
+
+if __name__ == '__main__':
+    main()
