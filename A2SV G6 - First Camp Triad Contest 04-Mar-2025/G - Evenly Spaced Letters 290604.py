154daf47a3e0e42a1c2261d24e6f15e9ac731659
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-04 10:00:29 +0300

    Created 'G - Evenly Spaced Letters' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 04-Mar-2025/G - Evenly Spaced Letters 290604.py
@@ -0,0 +1,41 @@
+# Problem: G - Evenly Spaced Letters - https://codeforces.com/gym/589822/problem/G
+
+import itertools
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def iinp():
+    return int(input().strip())
+
+def linp():
+    return list(map(int, input().strip().split()))
+
+def string():
+    return input().strip()
+
+def lsinp():
+    return input().strip().split()
+
+def digit():
+    return [int(i) for i in input().strip()]
+
+def character():
+    return list(input().strip())
+
+def solve(s):
+    return "".join(sorted(s))
+
+def main():
+    t = iinp()
+    for _ in range(t):
+        s = string()
+        print(solve(s))
+
+if __name__ == '__main__':
+    main()
