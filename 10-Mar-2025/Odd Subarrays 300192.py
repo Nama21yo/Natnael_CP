b25194f3b45c1e917f30a57811038565ad698797
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-10 15:57:54 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 10-Mar-2025/Odd Subarrays 300192.py
@@ -0,0 +1,50 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+import itertools
+from collections import Counter, defaultdict, deque
+from bisect import bisect_right, bisect_left
+from math import ceil, sqrt
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def iinp():
+    return int(input().strip())
+
+def linp():
+    return list(map(int, input().strip().split()))
+
+def string():
+    return input().strip()
+
+def lsinp():
+    return input().strip().split()
+
+def digit():
+    return [int(i) for i in input().strip()]
+
+def character():
+    return list(input().strip())
+
+def solve():
+    n = iinp()
+    nums = linp()
+    subarray = 0
+    i = 1
+    while i < n:
+        if nums[i - 1] > nums[i]:
+            subarray += 1
+            i += 2
+        else:
+            i += 1
+    return subarray
+
+def main():
+    t = iinp()
+    for  _ in range(t):
+        print(solve())
+
+if __name__ == '__main__':
+    main()
