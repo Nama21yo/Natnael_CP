6e1ed04e496015a7a43fcdd92a4ddad77e09bda4
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-22 12:02:35 +0300

    Created 'E - Strange Mirroring' by A2SV Hub

---
+++ A2SV G6 - Round #7 22-Mar-2025/E - Strange Mirroring 315493.py
@@ -0,0 +1,27 @@
+# Problem: E - Strange Mirroring - https://codeforces.com/gym/596141/problem/E
+
+
+from math import ceil, log2
+ 
+t = int(input())
+ 
+for _ in range(t):
+    s = input()
+    n = len(s)
+    q = int(input())
+    queries = list(map(int, input().split()))
+    
+    def find(k):
+        if 1 <= k <= n:
+            return s[k - 1]
+        
+        curr_level = ceil(log2(ceil(k / n)))
+        k = k - 2 ** (curr_level - 1) * n
+ 
+        return find(k).swapcase()
+    
+    ans = []
+    for k in queries:
+        ans.append(find(k))
+ 
+    print(*ans)
