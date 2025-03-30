0dd9a7049f725920acd4891db95f944c01664bfa
Natnael Yohanes <mataniayohanes@gmail.com>
2025-03-30 20:44:59 +0300

    Created 'N-Queens' by A2SV Hub

---
+++ 30-Mar-2025/N-Queens 322074.py
@@ -0,0 +1,33 @@
+# Problem: N-Queens - https://leetcode.com/problems/n-queens/description/
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        cols = set()
+        pos_diagonal = set() # / r + c is the same
+        neg_diagonal = set() # \ r - c is the same
+
+        board = [["."] * n for _ in range(n)]
+        valid_board = []
+        def backtrack(r):
+            if r == n:
+                valid_board.append(["".join(r) for r in board])
+                return
+            
+            for c in range(n):
+                if c in cols or (r + c) in pos_diagonal or (r - c) in neg_diagonal:
+                    continue
+                cols.add(c)
+                pos_diagonal.add(r + c)
+                neg_diagonal.add(r - c)
+                board[r][c] = "Q"
+                
+                backtrack(r + 1)
+
+                cols.remove(c)
+                pos_diagonal.remove(r + c)
+                neg_diagonal.remove(r - c)
+                board[r][c] = "."
+        backtrack(0)
+        return valid_board
+
+            
