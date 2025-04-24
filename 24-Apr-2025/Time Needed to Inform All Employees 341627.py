7880e7f73a8e547507c695327d249dad9af1f570
Natnael Yohanes <mataniayohanes@gmail.com>
2025-04-24 19:58:07 +0300

    Created 'Time Needed to Inform All Employees' by A2SV Hub

---
+++ 24-Apr-2025/Time Needed to Inform All Employees 341627.py
@@ -0,0 +1,15 @@
+# Problem: Time Needed to Inform All Employees - https://leetcode.com/problems/time-needed-to-inform-all-employees/
+
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        graph = defaultdict(list)
+        for i in range(len(manager)):
+            graph[manager[i]].append(i)
+        
+        def dfs(u):
+            ans = 0
+            for v in graph[u]:
+                ans = max(dfs(v) + informTime[u], ans)
+            return ans
+        
+        return dfs(headID)
