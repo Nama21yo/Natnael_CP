6682a4f6325408eede33eb8d3ea660d95d2ea570
Natnael Yohanes <mataniayohanes@gmail.com>
2025-05-10 14:00:35 +0300

    Created 'Longest Turbulent Subarray' by A2SV Hub

---
+++ 10-May-2025/Longest Turbulent Subarray 352253.py
@@ -0,0 +1,17 @@
+# Problem: Longest Turbulent Subarray - https://leetcode.com/problems/longest-turbulent-subarray/
+
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        down = up = res = 1
+        for i in range(1, len(arr)):
+            if arr[i] > arr[i - 1]:
+                down = up + 1
+                up = 1
+            elif arr[i] < arr[i - 1]:
+                up = down + 1
+                down = 1
+            else:
+                down = 1
+                up = 1
+            res = max(res, up, down)
+        return res
