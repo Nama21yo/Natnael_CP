26f69c23eb2048a9981352e337d33cc0d0d685f6
Natnael Yohanes <mataniayohanes@gmail.com>
2025-06-17 23:41:09 +0300

    Created 'Zero Array Transformation III' by A2SV Hub

---
+++ 17-Jun-2025/Zero Array Transformation III 367703.py
@@ -0,0 +1,30 @@
+# Problem: Zero Array Transformation III - https://leetcode.com/problems/zero-array-transformation-iii/description/
+
+class Solution:
+    def maxRemoval(self, nums: List[int], queries: List[List[int]]) -> int:
+        n = len(nums)
+        m = len(queries)
+        workload = [0] * (n + 1)
+
+        queries.sort()
+        available = []  # Max heap 
+        q_index = 0
+
+        for time in range(n):
+            if time > 0:
+                workload[time] += workload[time - 1]
+
+            while q_index < m and queries[q_index][0] == time:
+                heapq.heappush(available, -queries[q_index][1])
+                q_index += 1
+
+            while workload[time] < nums[time]:
+                if not available or -available[0] < time:
+                    return -1
+
+                workload[time] += 1
+                end_time = -heapq.heappop(available)
+                if end_time + 1 < len(workload):
+                    workload[end_time + 1] -= 1
+
+        return len(available)
