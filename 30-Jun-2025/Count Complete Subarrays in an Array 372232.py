cf5ec862490518c80f1b24357472a3d54b02b541
Natnael Yohanes <mataniayohanes@gmail.com>
2025-06-30 23:11:33 +0300

    Created 'Count Complete Subarrays in an Array' by A2SV Hub

---
+++ 30-Jun-2025/Count Complete Subarrays in an Array 372232.py
@@ -0,0 +1,17 @@
+# Problem: Count Complete Subarrays in an Array - https://leetcode.com/problems/count-complete-subarrays-in-an-array/
+
+class Solution:
+    def countCompleteSubarrays(self, nums):
+        distinct_elements = set(nums)
+        total_distinct = len(distinct_elements)
+        count = 0
+        n = len(nums)
+        
+        for i in range(n):
+            current_set = set()
+            for j in range(i, n):
+                current_set.add(nums[j])
+                if len(current_set) == total_distinct:
+                    count += (n - j)
+                    break
+        return count
